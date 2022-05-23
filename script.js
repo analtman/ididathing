0a3c2523a994836e340dc7e1261279a904f40ce6
A <romaniitedomum123@web.de>
2022-05-23 18:58:50 +0200

    addition game

--- script.js
+++ script.js
@@ -140,3 +140,27 @@ setInterval(() => {
   glitch.play();
   toggleFont();
 }, 12000);
+
+/*Game*/
+
+var character = document.getElementById("character");
+var block= document.getElementById("block");
+
+function jump() {
+    if(character.classList !="animate"){
+        character.classList.add("animate");
+    }
+    setTimeout(function() {
+        character.classList.remove("animate");
+    },500)
+}
+
+var checkDead = setInterval(function() {
+var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+if(blockLeft<20 && blockLeft>0 && characterTop>=130){
+    block.style.animation = "none";
+    block.style.display = "none";
+    alert("U Lost, Biatch!");
+}
+},10)
